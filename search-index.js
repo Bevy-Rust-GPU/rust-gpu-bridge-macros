aa62f81b490c53803688dc0ac5724851a5e8c637
Shfty <Shfty@users.noreply.github.com>
2023-04-23 03:26:26 +0000

    deploy: b6d71186efe47b9a6f1c182e8d1baf81df65bf7a

---
+++ search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"rust_gpu_bridge_macros":{"doc":"","t":[24],"n":["Named"],"q":["rust_gpu_bridge_macros"],"d":[""],"i":[0],"f":[0],"p":[]}\
+}');
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
---
+++ source-files.js
@@ -0,0 +1,4 @@
+var sourcesIndex = JSON.parse('{\
+"rust_gpu_bridge_macros":["",[],["lib.rs"]]\
+}');
+createSourceSidebar();
